7b18f77b6deaf8d6c0398f1c827bca400947d060
Stepa Ryazanof <stepa.ryazanoff@gmail.com>
2024-12-03 15:04:29 +0700

    Add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+
+  const config = new DocumentBuilder()
+    .setTitle('swagger api')
+    .setDescription('swagger for lesson')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 
