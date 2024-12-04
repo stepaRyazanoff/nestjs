a305d725da8be30eab72c08cd11cba9e2f88e58f
Stepa Ryazanof <stepa.ryazanoff@gmail.com>
2024-12-04 14:03:38 +0700

    Add logic create to user

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('get-all-users')
-  getUsers() {
-    return this.userService.getUsers();
+  @Post('create-user')
+  createUsers(@Body() dto: CreateUserDTO) {
+    console.log(dto);
+
+    return this.userService.createUser(dto);
   }
 }
